8e8f655d803119ce43cc1f0f5ed1c5c0119f9eea
critterwilson <wil15090@byui.edu>
2019-07-06 16:29:29 -0600

    first commit

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const pool = new Pool({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// This says that we want the function "getPerson" below to handle
-// any requests that come to the /getPerson endpoint
+// This says that we want the function "getMode below to handle
+// any requests that come to the /getMode endpoint
 app.get('/getMode', getMode);
 
-// Start the server running
+// Start the server
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
